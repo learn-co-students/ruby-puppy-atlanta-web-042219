9044b62ede384c29fcd55c62cffb272c6faafd49
github username <github email address>
2019-05-20 20:12:23 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,19 @@
-
 class Dog
-let!(:pluto) {Dog.new("Pluto")}
-let!(:fido) {Dog.new("Fido")}
-let!(:Maddy) {Dog.new("Maddy")}
-after(:each) do
-  Dog.class_variable_set(:@@all, [])
-end
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
+    puts @@all.map{ |dog| dog.name }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
 end
