22191c75d984e0b5c5a20d86aaeb1dfe1e249095
github username <github email address>
2019-05-26 19:42:41 -0400

    refresher

--- lib/dog2.rb
+++ lib/dog2.rb
@@ -1,22 +1,20 @@
 class Dog
-    attr_reader :name
+
+    attr_accessor :name 
 
     @@all = []
 
     def initialize(name)
-        @name = name
+        @name = name 
         @@all << self
     end
     
     def self.all
-        @@all.each { |dog| puts dog.name}
-     end 
+        @@all.each {|dog| puts dog.name}
+        
+    end 
 
     def self.clear_all
         @@all.clear
     end 
-
-    
-
-
 end 
